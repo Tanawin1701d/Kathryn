d8877cded64c0d5fd97006548f5d10405460b96e
tanawin <tanawin1701d@outlook.com>
2023-12-14 09:05:28 +0700

    [DRAFTING] add value representation for simulating and value component.

--- src/model/logicRep/valRep.cpp
+++ src/model/logicRep/valRep.cpp
@@ -2,4 +2,33 @@
 // Created by tanawin on 13/12/2566.
 //
 
+#include <cassert>
+#include <algorithm>
 #include "valRep.h"
+
+
+namespace kathryn{
+
+    ValRep::ValRep(int len): _len(len){
+        int alignedSizeInBit = len + (sizeof(ull) << 3) - 1;
+        int alignedSizeInByte = alignedSizeInBit >> 3;
+        int alighedSizeInUll  = alignedSizeInByte >> 3;
+
+        assert(alighedSizeInUll > 0);
+        lenValArr = alighedSizeInUll;
+        _val = new ull[lenValArr];
+        std::fill(_val, _val + lenValArr, (ull)0);
+    }
+
+    ValRep::~ValRep(){
+        delete[] _val;
+    }
+
+    ValRep::ValRep(const ValRep &rhs) {
+        _len = -1;
+        lenValArr = -1;
+        _val =
+    }
+
+
+}
--- src/model/logicRep/valRep.h
+++ src/model/logicRep/valRep.h
@@ -14,13 +14,47 @@ namespace kathryn{
      * this class is used to represent value the logic while simulating or building
      * model
      * */
+
+    /**
+     *
+     * shink mechanisim is used when inconsistent
+     * bit occur this new class will operate with
+     * new size based on shorter val representation
+     */
     class ValRep{
     private:
         size_t _len = -1;
-        int*  _val
+        int lenValArr = -1;
+        ull*  _val = nullptr;
 
     public:
+        explicit ValRep(int len);
+        ~ValRep();
 
+        ValRep(const ValRep& rhs);
+
+        ValRep& operator = (const ValRep& rhs);
+        ValRep& operator & (const ValRep& rhs);
+        ValRep& operator | (const ValRep& rhs);
+        ValRep& operator ^ (const ValRep& rhs);
+        ValRep& operator ~ ();
+        ValRep& operator << (const ValRep& rhs);
+        ValRep& operator >> (const ValRep& rhs);
+
+        ValRep& operator && (const ValRep& rhs);
+        ValRep& operator || (const ValRep& rhs);
+        ValRep& operator !  ();
+        ValRep& operator == (const ValRep& rhs);
+        ValRep& operator != (const ValRep& rhs);
+        ValRep& operator <  (const ValRep& rhs);
+        ValRep& operator <= (const ValRep& rhs);
+        ValRep& operator >  (const ValRep& rhs);
+        ValRep& operator >= (const ValRep& rhs);
+        ValRep& operator +  (const ValRep& rhs);
+        ValRep& operator -  (const ValRep& rhs);
+        ValRep& operator *  (const ValRep& rhs);
+        ValRep& operator /  (const ValRep& rhs);
+        ValRep& operator %  (const ValRep& rhs);
 
     };
 
