07527ec7577203eca2da6d1a7e4f525a5e5a1ee7
Tanawin Devaveja <tanawin1701d@outlook.com>
2024-03-22 11:51:50 +0700

    [Drafting] add dry Assign and debug nest mis match element.

---
+++ src/test/autoSim/testCase/simAutoTest22.cpp
@@ -0,0 +1,62 @@
+//
+// Created by tanawin on 22/1/2567.
+//
+
+#include "kathryn.h"
+#include "test/autoSim/simAutoInterface.h"
+#include "autoTestInterface.h"
+
+namespace kathryn{
+
+    class testSimMod22: public Module{
+    public:
+        makeReg(a, 32);
+        makeReg(m, 32);
+        makeReg(b, 32);
+
+        makeVal(c, 8, 8);
+        makeVal(d, 32, 10);
+
+
+        explicit testSimMod22(int x): Module(){}
+
+        void flow() override{
+
+            g(a,m,b) <<= g(c,d);
+            m(9) <<= 1;
+
+        }
+    };
+
+    static std::string vcdPath = "/media/tanawin/tanawin1701e/project2/Kathryn/KOut/simAutoTest22.vcd";
+    static std::string profilePath = "/media/tanawin/tanawin1701e/project2/Kathryn/KOut/profAutoTest22.vcd";
+
+
+    class sim22 :public SimAutoInterface{
+    public:
+
+        testSimMod22* _md;
+
+        sim22(testSimMod22* md):SimAutoInterface(22,
+                                              300,
+                                              vcdPath,
+                                              profilePath),
+                             _md(md)
+        {}
+
+    };
+
+
+    class Sim22TestEle: public AutoTestEle{
+    public:
+        explicit Sim22TestEle(int id): AutoTestEle(id){}
+        void start() override{
+            makeMod(d, testSimMod22, 1);
+            sim22 simulator((testSimMod22*) &d);
+            simulator.simStart();
+        }
+
+    };
+
+    Sim22TestEle ele22(-1);
+}
