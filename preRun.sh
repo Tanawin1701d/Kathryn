11900aae905160fc408b401bfddf6c76d2d29c36
tanawin <tanawin1701d@outlook.com>
2024-07-06 21:23:02 +0700

    [drafting] add tutorial example

---
+++ preRun.sh
@@ -0,0 +1,8 @@
+mkdir modelCompile/generated
+mkdir modelCompile/build
+mkdir KOut/genTest
+mkdir KOut/genTest/model
+mkdir KOut/genTest/output
+mkdir KOut/genTest/program
+mkdir KOut/genExample
+mkdir KOut/simpleTest
