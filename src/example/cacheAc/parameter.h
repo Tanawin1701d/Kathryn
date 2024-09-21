26102d18cfd80b359f08418debf5d8617740b8ae
tanawin <tanawin1701d@outlook.com>
2024-09-21 17:05:48 +0700

    [drafting] finish draft cache ac todo next test it.

--- src/example/cacheAc/parameter.h
+++ src/example/cacheAc/parameter.h
@@ -1,44 +1,38 @@
 //
-// Created by tanawin on 13/9/2024.
+// Created by tanawin on 20/9/2024.
 //
 
-#ifndef KATHRYN_PARAMETER_H
-#define KATHRYN_PARAMETER_H
+#ifndef KATHRYN_SRC_EXAMPLE_CACHEAC_PARAMETER_H
+#define KATHRYN_SRC_EXAMPLE_CACHEAC_PARAMETER_H
 
 
 #include<iostream>
 
-    namespace kathryn::cacheServer{
+namespace kathryn::cacheServer{
 
-        enum REPLACE_POL{
-            OVER_WRITE,
-            AVOID_CONFLICT
-        };
+    enum REPLACE_POL{
+        OVER_WRITE,
+        AVOID_CONFLICT
+    };
 
-        struct KV_PARAM{
-            int KEY_SIZE = 8; ///// key size if the cache
-            int VALUE_SIZE = 32; //////// value bitwidth
-            REPLACE_POL replacePol = OVER_WRITE;
+    struct KV_PARAM{
+        int KEY_SIZE = 8; ///// key size if the cache
+        int VALUE_SIZE = 32; //////// value bitwidth
+        REPLACE_POL replacePol = OVER_WRITE;
+    };
 
+    struct SERVER_PARAM{
+        KV_PARAM kvParam;
+        int      ingrQueueSize = 8;
+        int      prefixBit     = 2; ////// it mean key bit is set to 6 == 64 element perbank
+        ////// prefix bit must lower than key size
+    };
 
-        };
+    struct KV_DEBUG{
+        std::string key;
+        std::string value;
+    };
 
-        struct SERVER_PARAM{
-            KV_PARAM kvParam;
-            int      ingrQueueSize = 8;
-            int      prefixBit     = 2; ////// it mean key bit is set to 6 == 64 element perbank
-            ////// prefix bit must lower than key size
+}
 
-        };
-
-        struct KV_DEBUG{
-            std::string key;
-            std::string value;
-        };
-
-
-    }
-
-
-
-#endif //KATHRYN_PARAMETER_H
+#endif //KATHRYN_SRC_EXAMPLE_CACHEAC_PARAMETER_H
