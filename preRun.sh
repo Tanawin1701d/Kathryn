42d781a756814741760625f3c6eb284144975728
tanawin <tanawin1701d@outlook.com>
2024-11-05 16:18:24 +0700

    [drafting] rearrage file

--- preRun.sh
+++ preRun.sh
@@ -1,8 +1,2 @@
 mkdir modelCompile/generated
 mkdir modelCompile/build
-mkdir KOut/genTest
-mkdir KOut/genTest/model
-mkdir KOut/genTest/output
-mkdir KOut/genTest/program
-mkdir KOut/genExample
-mkdir KOut/simpleTest
